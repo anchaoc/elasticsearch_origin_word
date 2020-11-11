5575483babc744fec4b747891f7c5c409f29f7f7
安超 <240853850@qq.com>
2020-11-11 17:10:13 +0800

    Initial commit

---
+++ src/main/java/com/example/word/ElasticsearchOriginWordApplication.java
@@ -0,0 +1,13 @@
+package com.example.word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchOriginWordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchOriginWordApplication.class, args);
+    }
+
+}
